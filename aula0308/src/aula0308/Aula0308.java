f57699b3b94f34b8782e661d554ac53be676a75e
GEIZIELLE CRISTINA SILVA DE FARIA <geizielle.faria@alunos.sc.senac.br>
2021-08-03 21:41:32 -0300

    aula 03/08

---
+++ aula0308/src/aula0308/Aula0308.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula0308;
+
+/**
+ *
+ * @author geizielle.faria
+ */
+public class Aula0308 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] listnum = {1, 2, 3, 4};
+        int resp = 5 ;
+if (listnum[0] + listnum[1] == resp){
+System.out.print("Posição 0,1" ;
+if (listnum[0] + listnum[2] == resp){
+System.out.print("Posição 0,2" );
+if (listnum[0] + listnum[3] == resp){
+System.out.print("Posição 0,3" );
+if (listnum[1] + listnum[2] == resp){
+System.out.print("Posição 1,2" );
+if (listnum[1] + listnum[3] == resp){
+System.out.print("Posição 1,3" );
+if (listnum[2] + listnum[3] == resp){
+System.out.print("Posição 2,3" );
+}}}}}}
+        else {
+            System.out.print("erro");
+        }
+       
+    }
+    
+}
